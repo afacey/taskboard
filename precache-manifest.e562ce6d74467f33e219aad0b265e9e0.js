eb982e353e0b26b00cc80e3038da74b9602d782e
Andre <drefacey@gmail.com>
2020-09-25 21:47:52 -0400

    Updates

---
+++ precache-manifest.e562ce6d74467f33e219aad0b265e9e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8680bd0a0bdce786291a52b1d7b62476",
+    "url": "/andre-facey-project-five/index.html"
+  },
+  {
+    "revision": "5d4fe89e5bb051dd7e9b",
+    "url": "/andre-facey-project-five/static/css/main.5f669932.chunk.css"
+  },
+  {
+    "revision": "9b777e36133d1531b6c8",
+    "url": "/andre-facey-project-five/static/js/2.807ad5ae.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/andre-facey-project-five/static/js/2.807ad5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4fe89e5bb051dd7e9b",
+    "url": "/andre-facey-project-five/static/js/main.97b3adf5.chunk.js"
+  },
+  {
+    "revision": "c23edd4bb7cd21acdaae",
+    "url": "/andre-facey-project-five/static/js/runtime-main.3efebbe6.js"
+  }
+]);
