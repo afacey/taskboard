e4b8bd88cbd4ed84f26a9dc85dfde95540f13fd8
Andre <drefacey@gmail.com>
2020-09-26 12:06:03 -0400

    adjust styles

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import TaskItem from './TaskItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 const TaskList = props => {
   const { status, tasks, children } = props;
   return(
     <div className="taskList">
       <h2 className={`taskList__heading taskList__heading--${status} clearfix`}>
         {children} 
-        {tasks.length > 0 && <span class="taskList__count">{tasks.length}</span>}
+        {tasks.length > 0 && <span className="taskList__count">{tasks.length}</span>}
         <button className={`btn__taskList btn__taskList--add`}>+ Task</button>
         <button className="btn__taskList btn__taskList--menu"><FontAwesomeIcon icon={faEllipsisV} /></button>
       </h2>
