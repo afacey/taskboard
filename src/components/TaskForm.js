f94b76d5da9dfbfec37ff28629838cf5583f557a
Andre <drefacey@gmail.com>
2020-10-15 14:13:11 -0400

    move around taskform buttons

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -8,10 +8,10 @@ const TaskForm = props => {
     <h3 className="taskForm__heading">{type === 'edit' ? 'Edit Task' : 'New Task'}</h3>
     <form action="#" onSubmit={handleSubmit} onBlur={handleBlur} className="taskForm">
       <textarea className="taskForm__input" id={`taskFormInput_${id}`} name="taskFormInput" onChange={handleChange} value={taskValue}></textarea>
-      <button className="btn btn--green">Save</button>
+      {type === "edit" && <button type="button" onClick={removeTask} className="btn btn--red">Delete</button>}
       <button type="button" onClick={toggleForm} className="btn btn--orange">Cancel</button>
       {type === "add" && <button type="button" onClick={addTask} className="btn__task btn__task--add">Add</button>}
-      {type === "edit" && <button type="button" onClick={removeTask} className="btn btn--red">Delete</button>}
+      <button className="btn btn--green">Save</button>
     </form>  
     </>
   );
