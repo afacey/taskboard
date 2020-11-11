d78483aeda58823cdbda34f57dce4e6a94f433a2
Andre <drefacey@gmail.com>
2020-11-10 19:26:03 -0500

    convert TaskList component to use hooks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskItem from './TaskItem';
 import TaskForm from './TaskForm';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import autosize from 'autosize';
 
-class TaskList extends Component {
-  constructor(props) {
-    super(props);
+const TaskList = ({ status, tasks, addTask, editTask, removeTask, moveTask, clearTaskList }) => {
+  const [ isStaging, setIsStaging ] = useState(false);
+  const [ stagingTask, setStagingTask ] = useState("");
+  const [ menuEnabled, setMenuEnabled ] = useState(false);
 
-    this.state = {
-      isStaging: false,
-      stagingTask: "",
-      menuEnabled: false
-    }
+  // heading text for task status lists
+  const statusString = {
+    open: "Open",
+    inProgress: "In Progress",
+    complete: "Completed"
   }
-
-  // --------------------------- componentDidUpdate
-  componentDidUpdate() {
-    const taskInput = document.querySelector(`#taskFormInput_${this.props.status}`);
+  
+  // --------------------------- useEffect
+  useEffect(() => {
+    const taskInput = document.querySelector(`#taskFormInput_${status}`);
 
     // only go to the end of the text if the input is not already focused
     if (taskInput && taskInput !== document.activeElement) {
@@ -30,15 +31,15 @@ class TaskList extends Component {
       // focus on the input
       taskInput.focus();
       // set the input value
-      taskInput.value = this.state.stagingTask;
-    } 
-  }
+      taskInput.value = stagingTask;
+    }
+  })
 
   // --------------------------- toggleMenuEnabled
-  toggleMenuEnabled = () => this.setState({menuEnabled: !this.state.menuEnabled});
+  const toggleMenuEnabled = () => setMenuEnabled(!menuEnabled);
 
   // --------------------------- handleBlur
-  handleBlur = (evt) => {
+  const handleBlur = (evt) => {
     // implementation from https://gist.github.com/pstoica/4323d3e6e37e8a23dd59
     const currentTarget = evt.currentTarget;
 
@@ -47,45 +48,38 @@ class TaskList extends Component {
       // Check if the new activeElement is a child of the original container
       if (!currentTarget.contains(document.activeElement)) {
         // if new focused element is not contained in the form ... toggle out of staging a task
-        this.setState({isStaging: false});
+        setIsStaging(false);
       }
     }, 5);
   }
 
   // --------------------------- handleStagingTask
-  handleStagingTask = (e) => this.setState({stagingTask: e.target.value});
+  const handleStagingTask = (e) => setStagingTask(e.target.value);
   // --------------------------- toggleTaskStaging
-  toggleTaskStaging = () => this.setState({isStaging: !this.state.isStaging});
+  const toggleTaskStaging = () => setIsStaging(!isStaging);
 
   // --------------------------- handleAddTask
-  handleAddTask = (e) => {
+  const handleAddTask = (e) => {
     e.preventDefault();
 
     // prevent adding empty tasks
-    if (this.state.stagingTask) {
-
-      const { stagingTask: task } = this.state;
-      const { status } = this.props;
+    if (stagingTask) {
 
       // add task
-      this.props.addTask({
-        task,
+      addTask({
+        task: stagingTask,
         status
       })
 
       // reset staging states
-      this.setState({
-        isStaging: false, 
-        stagingTask: "",
-      });
+      setIsStaging(false);
+      setStagingTask("");
     }
     
   }
 
   // --------------------------- handleClearList
-  handleClearList = () => {
-    const { tasks, status, clearTaskList } = this.props;
-    
+  const handleClearList = () => {
     // if task lst has items
     if (tasks.length) {
 
@@ -102,95 +96,82 @@ class TaskList extends Component {
       clearTaskList(taskListItems, status)  
     }
     // toggle tasklist menu to false
-    this.setState({menuEnabled: false});
+    setMenuEnabled(false);
   }
 
   // clear stagingTask state
-  clearStagingTask = () => { this.setState({stagingTask: ""}); }
+  const clearStagingTask = () => { setStagingTask(""); }
   
-  // --------------------------- render
-  render() {
-    const { status, tasks, editTask, removeTask, moveTask } = this.props;
-    const {handleAddTask, toggleTaskStaging, handleStagingTask, handleClearList, clearStagingTask, handleBlur } = this;
-    const { menuEnabled, stagingTask } = this.state;
+  // --------------------------- return
 
-     // heading text for task status lists
-    const statusString = {
-      open: "Open",
-      inProgress: "In Progress",
-      complete: "Completed"
-    }
+  return(
+    <div className="taskList">
+      <div className={`taskList__header taskList__header--${status}`}>
+        <label htmlFor={`taskListMenuBtn--${status}`} className="srOnly">Click the button to toggle the task list menu to clear the task list's items</label>
+        <button 
+          id={`taskListMenuBtn--${status}`} className={menuEnabled ? "btn taskList__menuBtn taskList__menuBtn--active" : "btn taskList__menuBtn" } 
+          onClick={toggleMenuEnabled}>
+            <span className="srOnly">Toggle the task list's menu</span>
+          {!menuEnabled ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faTimes} /> }  
+        </button>
+        
+        <h2 className="taskList__headingText">
+          {/* Task List Name */}
+          {statusString[status]} 
+          {/* Task List Item Counter */}
+          {tasks.length > 0 && <span className="taskList__count">{tasks.length}</span>}
+        </h2>
 
-    return(
-      <div className="taskList">
-        <div className={`taskList__header taskList__header--${status}`}>
-          <label htmlFor={`taskListMenuBtn--${status}`} className="srOnly">Click the button to toggle the task list menu to clear the task list's items</label>
-          <button 
-            id={`taskListMenuBtn--${status}`} className={menuEnabled ? "btn taskList__menuBtn taskList__menuBtn--active" : "btn taskList__menuBtn" } 
-            onClick={this.toggleMenuEnabled}>
-              <span className="srOnly">Toggle the task list's menu</span>
-            {!menuEnabled ? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faTimes} /> }  
-          </button>
-          
-          <h2 className="taskList__headingText">
-            {/* Task List Name */}
-            {statusString[status]} 
-            {/* Task List Item Counter */}
-            {tasks.length > 0 && <span className="taskList__count">{tasks.length}</span>}
-          </h2>
-
-          {
-            // Toggle task list header icon depending if the menuEnabled is true
-            !this.state.menuEnabled 
-            ? 
-            <>
-              <label htmlFor={`taskListAddBtn--${status}`} className="srOnly">Click the button to toggle the add new task form</label>
-              <button id={`taskListAddBtn--${status}`} onClick={toggleTaskStaging} className={`btn taskList__addBtn`} disabled={this.state.isStaging ? 'disabled' : ''}>+ Task</button>
-            </>
-            : 
-            <>
-              <label htmlFor={`taskListClearBtn--${status}`} className="srOnly">Click the button to clear the task list's items</label>
-              <button id={`taskListClearBtn--${status}`} onClick={handleClearList} className={`btn btn--black taskList__clearBtn`} disabled={tasks.length ? '' : 'disabled' }>Clear List</button>
-            </>
-          }
-          
-          
-        </div>
-        <ul className="taskList__list">
-          { 
-          // render a TaskForm to add a new task for the task list
-            this.state.isStaging && 
-            <li className={`taskItem taskItem--${status}`}>
-              <TaskForm 
-                id={status}
-                taskValue={stagingTask}
-                handleSubmit={handleAddTask}
-                toggleForm={toggleTaskStaging}
-                handleBlur={handleBlur}
-                handleChange={handleStagingTask}
-                handleClear={clearStagingTask}
-              />
-            </li>
-          }
-          { 
-          // render the taskform items for the list
-            this.props.tasks.map(({key, task, status}) => (
-              <TaskItem 
-                key={key} 
-                id={key} 
-                task={task} 
-                status={status} 
-                editTask={editTask}
-                removeTask={removeTask} 
-                moveTask={moveTask} 
-              />)
-            )
-          }
-        </ul>
+        {
+          // Toggle task list header icon depending if the menuEnabled is true
+          !menuEnabled 
+          ? 
+          <>
+            <label htmlFor={`taskListAddBtn--${status}`} className="srOnly">Click the button to toggle the add new task form</label>
+            <button id={`taskListAddBtn--${status}`} onClick={toggleTaskStaging} className={`btn taskList__addBtn`} disabled={isStaging ? 'disabled' : ''}>+ Task</button>
+          </>
+          : 
+          <>
+            <label htmlFor={`taskListClearBtn--${status}`} className="srOnly">Click the button to clear the task list's items</label>
+            <button id={`taskListClearBtn--${status}`} onClick={handleClearList} className={`btn btn--black taskList__clearBtn`} disabled={tasks.length ? '' : 'disabled' }>Clear List</button>
+          </>
+        }
+        
+        
       </div>
-    )
-
-  }
+      <ul className="taskList__list">
+        { 
+        // render a TaskForm to add a new task for the task list
+          isStaging && 
+          <li className={`taskItem taskItem--${status}`}>
+            <TaskForm 
+              id={status}
+              taskValue={stagingTask}
+              handleSubmit={handleAddTask}
+              toggleForm={toggleTaskStaging}
+              handleBlur={handleBlur}
+              handleChange={handleStagingTask}
+              handleClear={clearStagingTask}
+            />
+          </li>
+        }
+        { 
+        // render the taskform items for the list
+          tasks.map(({key, task, status}) => (
+            <TaskItem 
+              key={key} 
+              id={key} 
+              task={task} 
+              status={status} 
+              editTask={editTask}
+              removeTask={removeTask} 
+              moveTask={moveTask} 
+            />)
+          )
+        }
+      </ul>
+    </div>
+  )
 
 }
 export default TaskList;
