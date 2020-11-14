55211181f0020031d282367fd3632e2614607485
Andre <drefacey@gmail.com>
2020-11-14 10:07:11 -0500

    Updates

---
+++ precache-manifest.78ab77cd6d9df64e7ffbd898105a325d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4629640112db91b1caaf45e1f4df30bc",
+    "url": "/taskboard/index.html"
+  },
+  {
+    "revision": "2d1039eff877c3d18a10",
+    "url": "/taskboard/static/css/main.34658b4e.chunk.css"
+  },
+  {
+    "revision": "dba2ccbeb4ecff87ce02",
+    "url": "/taskboard/static/js/2.d6bbeb73.chunk.js"
+  },
+  {
+    "revision": "1c49a115273fb9000d38d09c78dc007f",
+    "url": "/taskboard/static/js/2.d6bbeb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1039eff877c3d18a10",
+    "url": "/taskboard/static/js/main.1ec5c5bf.chunk.js"
+  },
+  {
+    "revision": "3de424a73134a1ab3667",
+    "url": "/taskboard/static/js/runtime-main.adae2a35.js"
+  }
+]);
